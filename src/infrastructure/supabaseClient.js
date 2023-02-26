a46e0d95b952b1f6678acdc8acf303a9dc3efdb3
RIZY101 <rizins@zins.dynu.com>
2023-02-26 11:05:51 -0800

    decided I will put supabase functions here
    Jacob you can move these later if you want

--- src/infrastructure/supabaseClient.js
+++ src/infrastructure/supabaseClient.js
@@ -3,3 +3,16 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'http://192.53.127.138:8000';
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function signInWithEmail(emailStr) {
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email: emailStr,
+    options: {
+      emailRedirectTo: 'http://sbomescrow.com/',
+    },
+  })
+}
+
+async function signOut() {
+  const { error } = await supabase.auth.signOut()
+}
