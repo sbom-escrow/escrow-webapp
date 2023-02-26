a7f29c7351cdedbb3a4db2d06715016127f2c25c
RIZY101 <rizins@zins.dynu.com>
2023-02-26 13:46:41 -0800

    update api

--- src/infrastructure/supabaseClient.js
+++ src/infrastructure/supabaseClient.js
@@ -4,15 +4,24 @@ const supabaseUrl = 'https://nfpdjrmzwsvwkhjgtupl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mcGRqcm16d3N2d2toamd0dXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc0NDYyMzksImV4cCI6MTk5MzAyMjIzOX0.R4GbnfPXgcxmEBJid2zRLOY7SteiBXPOXm2cVKoYjYQ';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function signInWithEmail(emailStr) {
-  const { data, error } = await supabase.auth.signInWithOtp({
-  email: emailStr
+async function signInWithEmail(emailStr, passwStr) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+  email: emailStr,
+  password: passwStr
 })
-  console.log(error);
+  return { data, error };
+}
+
+async function signUp(emailStr, passwStr) {
+  const { data, error } = await supabase.auth.signUp({
+  email: emailStr,
+  password: passwStr
+})
+  return { data, error };
 }
 
 async function signOut() {
   const { error } = await supabase.auth.signOut()
 }
 
-export { signOut, signInWithEmail };
+export { signOut, signUp, signInWithEmail };
