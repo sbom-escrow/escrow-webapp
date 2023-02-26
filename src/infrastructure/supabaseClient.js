b0b91b3587dc5aace5b5324efee3fc6325c2ece7
RIZY101 <rizins@zins.dynu.com>
2023-02-26 12:51:57 -0800

    triying normal auth

--- src/infrastructure/supabaseClient.js
+++ src/infrastructure/supabaseClient.js
@@ -4,10 +4,11 @@ const supabaseUrl = 'http://192.53.127.138:8000';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function signInWithEmail(emailStr) {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: emailStr
-  })
+async function signInWithEmail(emailStr, passwdStr) {
+  const { data, error } = await supabase.auth.signUp({
+  email: emailStr,
+  password: passwdStr
+})
   console.log(error);
 }
 
