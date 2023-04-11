1836494ca21f14a34cae166e11d23207ee68f85a
Jacob Glad <jacob.q.glad@gmail.com>
2023-04-11 15:49:33 -0600

    Added mock smart contract stuff

--- src/components/SbomView.js
+++ src/components/SbomView.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment, useState } from 'react';
 import axios from 'axios';
 import Sbom from '../infrastructure/Sbom';
 import DangerousSoftware from '../data/DangerousSoftware'
-import { Collapse, ListGroup, ListGroupItem, Table, Button} from 'reactstrap';
+import { Collapse, ListGroup, ListGroupItem, Table, Button,Modal,ModalBody,ModalHeader} from 'reactstrap';
 
 class SbomView extends Component {
   constructor(props){
@@ -13,13 +13,29 @@ class SbomView extends Component {
         version : props.sbom.version,
         vendor : props.sbom.vendor
       }),
-      collapse: false
+      collapse: false,
+      modal:false,
+      modalVulnerableComponent:null,
+      collapseCode: false,
+      collapseResult:false,
     }
     this.toggle = this.toggle.bind(this);
-  }
+    this.toggleModal = this.toggleModal.bind(this);    
+    this.toggleCollapseCode = this.toggleCollapseCode.bind(this);
+    this.toggleCollapseResult = this.toggleCollapseResult.bind(this);
+  }  
 
  toggle() {
     this.setState({ collapse: !this.state.collapse });
+  }
+  toggleCollapseCode() {
+    this.setState({ collapseCode: !this.state.collapseCode });
+  }
+  toggleCollapseResult() {
+    this.setState({ collapseResult: !this.state.collapseResult });
+  }
+  toggleModal(component) {
+    this.setState({ modal: !this.state.modal,modalVulnerableComponent: component});
   }
 
   render() {
@@ -60,6 +76,7 @@ class SbomView extends Component {
             <th>Component</th>
             <th>Version</th>
             <th>CVE</th>
+            <th>Smart Contract</th>
           </thead>
           <tbody>
             {vulnerableComponents.map((vulnerableComponent) => (
@@ -67,6 +84,7 @@ class SbomView extends Component {
                   <td>{vulnerableComponent.name}</td>
                   <td>{vulnerableComponent.version}</td>
                   <td><a target="_blank" href={vulnerableComponent.url}>{vulnerableComponent.cve}</a></td>
+                  <td><Button color="success" onClick={() => this.toggleModal(vulnerableComponent)}>View</Button></td>
                 </tr>     
               ))}     
           </tbody>          
@@ -75,6 +93,33 @@ class SbomView extends Component {
         <Collapse isOpen={!this.state.collapse} style={{paddingTop:'1em'}}>
           <hr/>
         </Collapse>
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal} size='xl'>
+          <ModalHeader toggle={this.toggleModal}>Smart Contract for {this.state.modalVulnerableComponent ? this.state.modalVulnerableComponent.name : ''}</ModalHeader>
+          <ModalBody>
+            <h4>
+              <span>Code</span>
+              <Button color="primary" onClick={this.toggleCollapseCode} style={{ float:'right'}}>{this.state.collapseCode ? 'Hide' : 'Show' }</Button>
+            </h4>
+            <hr/>
+            <Collapse isOpen={this.state.collapseCode}>
+              <code style={{whiteSpace: 'break-spaces'}}>
+              {DangerousSoftware.contractCode}
+              </code>
+              <hr/>
+            </Collapse>
+            <h4>
+              <span>Result</span>
+              <Button color="primary" onClick={this.toggleCollapseResult} style={{ float:'right'}}>{this.state.collapseResult ? 'Hide' : 'Show' }</Button>
+            </h4>
+            <hr/>
+            <Collapse isOpen={this.state.collapseResult}>
+              <code style={{whiteSpace: 'break-spaces'}}>
+              {DangerousSoftware.contractResult}
+              </code>
+              <hr/>
+            </Collapse>
+          </ModalBody>
+        </Modal>
         </Fragment>
       }
         {!vulnerableComponents &&
@@ -83,7 +128,7 @@ class SbomView extends Component {
           <div style={{margin:'20px',textAlign:'center', fontSize: 'x-large'}}>This Software is Safe!</div>
           </Fragment>
         }
-        </div>
+        </div>        
       </Fragment>
     );
   }
